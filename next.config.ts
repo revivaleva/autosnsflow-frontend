edbad4dbcd52b4ad973d59c818203c5b9149873a
revivaleva <matanokei@gmail.com>
2025-08-07 12:40:46 +0900

    setting change

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 本番ビルド時にESLintエラーを無視する
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
