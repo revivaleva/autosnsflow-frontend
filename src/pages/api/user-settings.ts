eb02c524488194861f90f3d1599794ae58f99130
revivaleva <matanokei@gmail.com>
2025-08-08 20:29:39 +0900

    aws api add

--- src/pages/api/user-settings.ts
+++ src/pages/api/user-settings.ts
@@ -12,9 +12,9 @@ const client = new DynamoDBClient({
   }
 });
 
-  console.log("accessKeyId:", process.env.AUTOSNSFLOW_ACCESS_KEY_ID);
-  console.log("secretAccessKey:", process.env.AUTOSNSFLOW_SECRET_ACCESS_KEY);
-  
+  console.log("accessKeyId:", process.env.AUTOSNSFLOW_ACCESS_KEY_ID!);
+  console.log("secretAccessKey:", process.env.AUTOSNSFLOW_SECRET_ACCESS_KEY!);
+  console.log("region:", process.env.NEXT_PUBLIC_AWS_REGION);
 // Cognito JWTの検証（シンプルなデコードのみ。検証までやる場合はJWKも必要です）
 function getUserIdFromToken(token?: string): string | null {
   if (!token) return null
