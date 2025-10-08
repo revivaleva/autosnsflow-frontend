c4a8a8c8619d86853501e7ae4460a0a05552de5c
revivaleva <matanokei@gmail.com>
2025-10-08 23:47:08 +0900

    chore(ui): update layout and document; add T-Booster icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,11 @@ import React from 'react';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <link rel="icon" href="/T-Booster_icon.ico" />
+        <link rel="apple-touch-icon" href="/T-Booster_icon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/T-Booster_icon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
